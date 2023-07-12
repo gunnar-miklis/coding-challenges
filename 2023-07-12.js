ed9031a30a5589a2039f0dea3c662dd28c787e00
gunnar-miklis <gunnar.miklis@gmail.com>
2023-07-12 17:01:45 +0200

    100. Completed Kata 🥳🎉

--- 2023-07-12.js
+++ 2023-07-12.js
@@ -1,4 +1,5 @@
 // https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/javascript
+// My Total Completed Kata: 100 🥳
 
 // my solution 1: using .includes()
 function scramble( str1, str2 ) {
