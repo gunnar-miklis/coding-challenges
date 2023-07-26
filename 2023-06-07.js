5490fb447ca98d784ae2cd3c5e0e2c8c5d683e1e
gunnar-miklis <gunnar.miklis@gmail.com>
2023-07-26 12:40:52 +0200

    solved

--- 2023-06-07.js
+++ 2023-06-07.js
@@ -1,24 +1,29 @@
 // https:// www.codewars.com/kata/550498447451fbbd7600041c/train/javascript
-// FIXME: Tests Passed: 212 Failed: 2
+
 function comp( a, b ) {
 	if ( !a || !b ) return false;
-	const aSquared = a.map( ( num ) => num*num );
-	const bCompared = b.map( ( square ) => aSquared.includes( square ) );
-	return bCompared.every( ( el ) => el );
+	const copyA = [ ...a ];
+	const copyB = [ ...b ];
+	copyA.sort( ( a, b ) => a - b );
+	copyB.sort( ( a, b ) => a - b );
+	return copyB.every( ( el, i ) => el === copyA[i]**2 );
 }
-// COMMENT: this test case won't pass...
-//	surely, because I check if it "includes",
-//	and yes it does include obviously, but it does not match the number of appearance
-const a1 = [2, 2, 3]; // 4, 4, 9 != 4, 9, 9
-const a2 = [4, 9, 9];
-console.log( comp( a1, a2 ) ); // expect: false
+console.log( comp( // :>> true
+	[ 121, 144, 19, 161, 19, 144, 19, 11 ],
+	[ 11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19 ] ),
+);
+console.log( comp( // :>> false
+	[ 2, 2, 3 ],
+	[ 4, 9, 9 ] ),
+);
+console.log( comp( // :>> true
+	[ 6, 0, 2, 8, 3, 4, 7, 1, 2, 0, 10, 7, 3, 2, 5, 8, 2 ],
+	[ 9, 4, 0, 36, 4, 0, 16, 64, 49, 49, 25, 100, 4, 1, 64, 4, 9 ] ),
+);
 
 
-
-
-
-// shuffle an array
+// shuffle an copyAy
 function shuffle( arr ) {
 	return arr.sort( ( a, b ) => Math.floor( Math.random() * a ) - b );
 }
-console.log( shuffle( [1, 2, 3, 4, 5, 6, 7] ) );
+console.log( shuffle( [ 1, 2, 3, 4, 5, 6, 7 ] ) );
